f69bd65e2533ab8d7229529d863d45382b84e481
Alperen Karslı <alperenkarsliceng@gmail.com>
2023-11-13 16:58:58 +0300

    yeni commandlar ve online listesi eklendi

--- Client.java
+++ Client.java
@@ -1,6 +1,10 @@
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 
 public class Client {
     public static void main(String[] args) {
@@ -18,23 +22,43 @@ public class Client {
             String username = scanner.nextLine();
             out.println(username);
 
-            Thread messageThread = new Thread(() -> {
-                String message;
+            ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+            // Asenkron görev 1 - Server'dan gelen mesajları oku ve yazdır
+            executorService.submit(() -> {
                 try {
+                    String message;
                     while ((message = in.readLine()) != null) {
-                        System.out.println(message);
+                        if (message.startsWith("Online users:")) {
+                            System.out.println("Online users: " + message.substring("Online users:".length()));
+                        } else {
+                            System.out.println(message);
+                        }
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
+                } finally {
+                    executorService.shutdown(); // Hata durumunda executorService'yi kapat
                 }
             });
-            messageThread.start();
 
-            String userInput;
-            while ((userInput = scanner.nextLine()) != null) {
-                out.println(userInput);
-            }
-        } catch (IOException e) {
+            // Asenkron görev 2 - Kullanıcının girişlerini Server'a gönder
+            executorService.submit(() -> {
+                try {
+                    String userInput;
+                    while ((userInput = scanner.nextLine()) != null) {
+                        out.println(userInput);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    executorService.shutdown(); // Kullanıcı giriş yapmayı durdurduğunda executorService'yi kapat
+                }
+            });
+
+            // executorService'yi kapatmak için bekleyelim
+            executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
     }
--- Server.java
+++ Server.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class Server {
     private List<PrintWriter> clientWriters = new ArrayList<>();
+    private List<String> connectedUsers = new ArrayList<>();
 
     public static void main(String[] args) {
         new Server().startServer(12345);
@@ -19,7 +20,12 @@ public class Server {
                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
                 clientWriters.add(out);
 
-                ClientHandler clientHandler = new ClientHandler(clientSocket, out);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                String username = in.readLine();
+                connectedUsers.add(username);
+                broadcast(username + " has joined the chat. ");
+
+                ClientHandler clientHandler = new ClientHandler(clientSocket, out, in, username);
                 Thread clientThread = new Thread(clientHandler);
                 clientThread.start();
             }
@@ -31,32 +37,73 @@ public class Server {
     private class ClientHandler implements Runnable {
         private Socket socket;
         private PrintWriter out;
+        private BufferedReader in;
         private String username;
 
-        public ClientHandler(Socket socket, PrintWriter out) {
+        public ClientHandler(Socket socket, PrintWriter out, BufferedReader in, String username) {
             this.socket = socket;
             this.out = out;
+            this.in = in;
+            this.username = username;
         }
-
         @Override
         public void run() {
-            try (
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))
-            ) {
-                this.username = in.readLine();
-                broadcast(username + " has joined the chat.");
+            try {
+                // Örneğin, bağlantının IP adresini almak için:
+                String clientIP = socket.getInetAddress().getHostAddress();
+                System.out.println(username + " is connected from IP: " + clientIP);
+
+                // Yeni kullanıcı bağlandığında tüm kullanıcılara online kullanıcı listesini gönder
+                broadcast("Online users: " + String.join(", ", connectedUsers));
 
                 String message;
                 while ((message = in.readLine()) != null) {
-                    broadcast(username + ": " + message);
+                    // Kullanıcının kendi mesajını göndermemesi için kontrol ekle
+                    if (message.equals("/online")) {
+                        // Kullanıcı "/online" yazdığında o anda online olan kullanıcıları gönder
+                        out.println("Online users: " + String.join(", ", connectedUsers));
+                    } else if (message.equals("/help")) {
+                        // Kullanıcı "/help" yazdığında yardım mesajını gönder
+                        out.println("Available commands: /online, /quit, /help, /private <username> <message>, /kick <username> <reason>");
+                    } else if (message.startsWith("/kick")) {
+                        String[] tokens = message.split(" ");
+                        String receiver = tokens[1];
+                        String reason = tokens[2];
+                        try {
+                            PrintWriter receiverWriter = clientWriters.get(connectedUsers.indexOf(receiver));
+                            receiverWriter.println("You have been kicked by " + username + " for " + reason);
+                            receiverWriter.close();
+                        } catch (Exception e) {
+                            out.println("User " + receiver + " is not online.");
+                        }
+                    } else if (message.equals("/admin")) {
+                        // Kullanıcı "/admin" yazdığında admin mesajını gönder
+                        out.println("You are an admin!");
+                    } else if (message.equals("/quit")) {
+                        // Kullanıcı "/quit" yazdığında bağlantıyı kapat
+                        out.println("Bye bye!");
+                        break;
+                    } else if (message.startsWith("/private")) {
+                        String[] tokens = message.split(" ");
+                        String receiver = tokens[1];
+                        String privateMessage = tokens[2];
+                        try {
+                            PrintWriter receiverWriter = clientWriters.get(connectedUsers.indexOf(receiver));
+                            receiverWriter.println(username + " (private): " + privateMessage);
+                        } catch (Exception e) {
+                            out.println("User " + receiver + " is not online.");
+                        
+                    }
+                    } else if (!message.startsWith(username + ":")) {
+                        broadcast(username + ": " + message);
+                    }
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
+                connectedUsers.remove(username);
                 clientWriters.remove(out);
-                if (username != null) {
-                    broadcast(username + " has left the chat.");
-                }
+                broadcast(username + " has left the chat. Online users: " + String.join(", ", connectedUsers));
             }
         }
     }
