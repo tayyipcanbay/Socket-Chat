1fb15d117b6c6ce0cdb428d1a130c9a7201ebede
Alperen Karslı <alperenkarsliceng@gmail.com>
2023-12-11 09:47:11 +0300

    Kod Açıklamaları

--- Client.java
+++ Client.java
@@ -5,33 +5,41 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-
 public class Client {
     public static void main(String[] args) {
+        // Client sınıfının nesnesi oluşturulur ve startClient metodu çağrılarak sunucu adresi ve port numarası belirtilir
         new Client().startClient("localhost", 12345);
     }
 
+    // Client'ın sunucu ile iletişimini başlatan metod
     public void startClient(String serverAddress, int serverPort) {
         try (
+            // Socket, PrintWriter ve BufferedReader try-with-resources bloğu içinde tanımlanır
             Socket socket = new Socket(serverAddress, serverPort);
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             Scanner scanner = new Scanner(System.in)
         ) {
+            // Kullanıcıdan kullanıcı adı alınır ve sunucuya gönderilir
             System.out.print("Enter your username: ");
             String username = scanner.nextLine();
             out.println(username);
 
+            // Asenkron görevleri yönetmek için ExecutorService oluşturulur
             ExecutorService executorService = Executors.newFixedThreadPool(2);
 
-            // Asenkron görev 1 - Server'dan gelen mesajları oku ve yazdır
+            // Asenkron görev 1 - Sunucudan gelen mesajları oku ve yazdır
             executorService.submit(() -> {
                 try {
                     String message;
                     while ((message = in.readLine()) != null) {
+                        // Sunucudan gelen mesajı işler
+
                         if (message.startsWith("Online users:")) {
+                            // Eğer mesaj "Online users:" ile başlıyorsa, çevrimiçi kullanıcı listesini gösterir
                             System.out.println("Online users: " + message.substring("Online users:".length()));
                         } else {
+                            // Diğer durumlarda gelen mesajı doğrudan ekrana yazdırır
                             System.out.println(message);
                         }
                     }
@@ -42,11 +50,12 @@ public class Client {
                 }
             });
 
-            // Asenkron görev 2 - Kullanıcının girişlerini Server'a gönder
+            // Asenkron görev 2 - Kullanıcının girişlerini sunucuya gönder
             executorService.submit(() -> {
                 try {
                     String userInput;
                     while ((userInput = scanner.nextLine()) != null) {
+                        // Kullanıcının girişini sunucuya gönderir
                         out.println(userInput);
                     }
                 } catch (Exception e) {
--- Server.java
+++ Server.java
@@ -4,27 +4,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Server {
+    // Bağlı istemcilerin PrintWriter nesnelerini tutan bir liste
     private List<PrintWriter> clientWriters = new ArrayList<>();
+    // Bağlı olan kullanıcı adlarını tutan bir liste
     private List<String> connectedUsers = new ArrayList<>();
 
     public static void main(String[] args) {
+        // Sunucu nesnesi oluşturulur ve belirtilen bağlantı noktası ile başlatılır
         new Server().startServer(12345);
     }
 
+    // Sunucuyu başlatan metod
     public void startServer(int port) {
         System.out.println("Chat Server is running...");
 
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             while (true) {
+                // Yeni bir istemci bağlantısı kabul edilir
                 Socket clientSocket = serverSocket.accept();
+                // Istemciye mesaj göndermek için PrintWriter oluşturulur ve listeye eklenir
                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
                 clientWriters.add(out);
 
+                // Istemciden gelen kullanıcı adı okunur ve listeye eklenir
                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                 String username = in.readLine();
                 connectedUsers.add(username);
+                // Yeni kullanıcı bağlandığında tüm kullanıcılara online kullanıcı listesini gönderen metodu çağırır
                 broadcast(username + " has joined the chat. ");
 
+                // Yeni bir istemci için bir ClientHandler oluşturulur ve bu işlemleri yeni bir thread'da çalıştırır
                 ClientHandler clientHandler = new ClientHandler(clientSocket, out, in, username);
                 Thread clientThread = new Thread(clientHandler);
                 clientThread.start();
@@ -34,73 +43,85 @@ public class Server {
         }
     }
 
+    // Her bir istemci için çalışacak olan ClientHandler sınıfı
     private class ClientHandler implements Runnable {
         private Socket socket;
         private PrintWriter out;
         private BufferedReader in;
         private String username;
 
+        // ClientHandler yapıcısı
         public ClientHandler(Socket socket, PrintWriter out, BufferedReader in, String username) {
             this.socket = socket;
             this.out = out;
             this.in = in;
             this.username = username;
         }
+
+        // Runnable arayüzünden implement edilen run metodu
         @Override
         public void run() {
             try {
-                // Örneğin, bağlantının IP adresini almak için:
+                // Istemcinin IP adresini alır
                 String clientIP = socket.getInetAddress().getHostAddress();
                 System.out.println(username + " is connected from IP: " + clientIP);
 
-                // Yeni kullanıcı bağlandığında tüm kullanıcılara online kullanıcı listesini gönder
+                // Yeni kullanıcı bağlandığında tüm kullanıcılara online kullanıcı listesini gönderen metodu çağırır
                 broadcast("Online users: " + String.join(", ", connectedUsers));
 
                 String message;
+                // Istemciden gelen mesajları dinleyen döngü
                 while ((message = in.readLine()) != null) {
-                    // Kullanıcının kendi mesajını göndermemesi için kontrol ekle
+                    // Kullanıcının kendi mesajını göndermemesi için kontrol
                     if (message.equals("/online")) {
-                        // Kullanıcı "/online" yazdığında o anda online olan kullanıcıları gönder
+                        // Kullanıcı "/online" yazdığında o anda online olan kullanıcıları gönderen metodu çağırır
                         out.println("Online users: " + String.join(", ", connectedUsers));
                     } else if (message.equals("/help")) {
-                        // Kullanıcı "/help" yazdığında yardım mesajını gönder
+                        // Kullanıcı "/help" yazdığında yardım mesajını gönderen metodu çağırır
                         out.println("Available commands: /online, /quit, /help, /private <username> <message>, /kick <username> <reason>");
                     } else if (message.startsWith("/kick")) {
+                        // Kullanıcı "/kick" yazdığında işlemleri gerçekleştiren blok
                         String[] tokens = message.split(" ");
                         String receiver = tokens[1];
                         String reason = tokens[2];
                         try {
+                            // Kullanıcıyı kickleme işlemi
                             PrintWriter receiverWriter = clientWriters.get(connectedUsers.indexOf(receiver));
                             receiverWriter.println("You have been kicked by " + username + " for " + reason);
                             receiverWriter.close();
                         } catch (Exception e) {
+                            // Kullanıcı çevrimiçi değilse uyarı mesajı gönderir
                             out.println("User " + receiver + " is not online.");
                         }
                     } else if (message.equals("/admin")) {
-                        // Kullanıcı "/admin" yazdığında admin mesajını gönder
+                        // Kullanıcı "/admin" yazdığında admin mesajını gönderir
                         out.println("You are an admin!");
                     } else if (message.equals("/quit")) {
-                        // Kullanıcı "/quit" yazdığında bağlantıyı kapat
+                        // Kullanıcı "/quit" yazdığında bağlantıyı kapatır
                         out.println("Bye bye!");
                         break;
                     } else if (message.startsWith("/private")) {
+                        // Özel mesaj gönderme işlemleri
                         String[] tokens = message.split(" ");
                         String receiver = tokens[1];
                         String privateMessage = tokens[2];
                         try {
+                            // Özel mesajı alıcıya gönderir
                             PrintWriter receiverWriter = clientWriters.get(connectedUsers.indexOf(receiver));
                             receiverWriter.println(username + " (private): " + privateMessage);
                         } catch (Exception e) {
+                            // Alıcı çevrimiçi değilse uyarı mesajı gönderir
                             out.println("User " + receiver + " is not online.");
-                        
-                    }
+                        }
                     } else if (!message.startsWith(username + ":")) {
+                        // Diğer durumlarda mesajı tüm kullanıcılara yayınlayan metodu çağırır
                         broadcast(username + ": " + message);
                     }
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
+                // Kullanıcı çıkış yaptığında yapılacak temizlik işlemleri
                 connectedUsers.remove(username);
                 clientWriters.remove(out);
                 broadcast(username + " has left the chat. Online users: " + String.join(", ", connectedUsers));
@@ -108,6 +129,7 @@ public class Server {
         }
     }
 
+    // Tüm istemcilere mesaj yayınlayan metot
     private void broadcast(String message) {
         for (PrintWriter writer : clientWriters) {
             writer.println(message);
